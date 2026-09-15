9099fa3112af333c1fb84e0416ada365cc5590f6
Maiharshit07 <achp2509@gmail.com>
2026-09-15 17:26:44 +0530

    cors fix

--- server/src/app.js
+++ server/src/app.js
@@ -61,7 +61,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma"],
   }),
 );
 app.use(express.json({ limit: "1mb" }));
