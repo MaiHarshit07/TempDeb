53b63dcb4137b1b57bddd3a89136a15de7d52701
Maiharshit07 <achp2509@gmail.com>
2026-09-15 17:25:11 +0530

    cors prefix error fixed

--- server/src/app.js
+++ server/src/app.js
@@ -61,7 +61,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
   }),
 );
 app.use(express.json({ limit: "1mb" }));
